5be9b60c5c2d5941dae1839872e63d6c14cadbdc
Roman Devyatov <roman_devyatov@epam.com>
2022-05-09 18:21:51 +0300

    [LP-6] added equals and hash code function, records are equal if urls are equal

--- src/main/java/com/rd/models/HistoryRecord.java
+++ src/main/java/com/rd/models/HistoryRecord.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -16,5 +18,18 @@ public class HistoryRecord {
     @Override
     public String toString() {
         return url + HISTORY_RECORD_DELEMITER + date;
+    }
+
+    @Override
+    public boolean equals(Object o) { // records are equal if they have the same url
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HistoryRecord that = (HistoryRecord) o;
+        return url.equals(that.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url);
     }
 }
