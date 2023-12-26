1ad291e0ff9d9805366bfa8b03d83e49facbedfe
Roman_Devyatov <romandevyatov98@gmail.com>
2023-12-26 04:29:20 +0300

    [LP-9] added new bash type for general run, default args for all. Two types: general and from config

--- execCopy/startCopyMaker.bat
+++ execCopy/startCopyMaker.bat
@@ -1,1 +1,3 @@
-start javaw -jar HistoryCopyMaker-1.0-SNAPSHOT.jar <path_to_working_directory> <config_path>
+start javaw -jar HistoryCopyMaker-1.0-SNAPSHOT.jar c <path_to_working_directory> <config_path>
+
+start javaw -jar HistoryCopyMaker-1.0-SNAPSHOT.jar g <path_to_working_directory> <browser_type> <logging_option> <path_to_db_history_overwritten>
